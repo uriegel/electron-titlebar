0795ae5c023378bf8fd40fc316b6d7ac4a0e4f14
Uwe <uriegel@hotmail.de>
2021-10-20 07:41:00 +0200

    New menubar version

--- components/Menubar.js
+++ components/Menubar.js
@@ -29,6 +29,7 @@ class Menubar extends HTMLElement {
                     list-style: none;
                     padding: 0;
                     margin: 0;
+                    height: 100%;
                 }
                 .invisible {
                     display: none;
@@ -283,6 +284,7 @@ class Submenu extends HTMLElement {
                 #submenu {
                     display: none;
                     position: absolute;
+                    z-index: 10000;
                 }
                 .selected #submenu {
                     display: block;
